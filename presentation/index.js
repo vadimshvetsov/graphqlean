d90af379575830cd2208cd8624da92037c9407c8
Vadim Shvetsov <vadim@shvetsov.io>
2019-01-30 02:06:39 +0300

    Add almost all GraphQL code and Notes

--- presentation/index.js
+++ presentation/index.js
@@ -7,6 +7,7 @@ import {
   Heading,
   Image,
   Layout,
+  Link,
   List,
   ListItem,
   Notes,
@@ -29,7 +30,6 @@ import 'styles/fonts.css';
 import 'styles/dracula-prism.css';
 
 const images = {
-  santa: require('images/santa.png'),
   psycho: require('images/psycho.gif'),
   redux: require('images/redux.gif'),
   graphql: require('images/graphql.gif'),
@@ -37,6 +37,8 @@ const images = {
   graphqlLogo: require('images/graphql-logo.svg'),
   easy: require('images/easy.gif'),
   hard: require('images/hard.gif'),
+  mls: require('images/mls.png'),
+  apollo: require('images/apollo.svg'),
 };
 
 const codeSamples = {
@@ -48,6 +50,11 @@ const codeSamples = {
   component: require('code/component.example'),
   query: require('code/query.example'),
   queryComponent: require('code/query-component.example'),
+  mutation: require('code/mutation.example'),
+  mutationComponent: require('code/mutation-component.example'),
+  pagination: require('code/pagination.example'),
+  cache: require('code/cache.example'),
+  localState: require('code/local-state.example'),
 };
 
 preloader(images);
@@ -72,7 +79,7 @@ const customTheme = {
       ...theme.screen.components,
       syntax: {
         ...theme.screen.components.syntax,
-        fontSize: '2em',
+        fontSize: '1.5em',
       },
     },
   },
@@ -101,10 +108,29 @@ const Presentation = () => (
         <ListItem margin="15px 0">Уменьшение связанности сервера и клиента</ListItem>
         <ListItem>Боли взаимодействия Redux с сервером</ListItem>
         <ListItem margin="15px 0">Решения GraphQL</ListItem>
-        <ListItem>Внедрение GraphQL в приложение клинеров</ListItem>
-        <ListItem margin="15px 0 0 0">Плавная миграция с Redux на GraphQL</ListItem>
+        <ListItem>Плавная миграция с Redux на GraphQL</ListItem>
+        <ListItem margin="15px 0 0 0">Внедрение GraphQL в приложение клинеров</ListItem>
       </List>
     </Slide>
+    <Slide bgColor="primary">
+      <Heading size={3} textColor="secondary" margin="0 0 50px 0">
+        MLS Case Study
+      </Heading>
+      <Image src={images.mls} width={1000} margin="0 0 30px"/>
+      <Link
+        href="https://blog.apollographql.com/reducing-our-redux-code-with-react-apollo-5091b9de9c2a"
+        textColor="secondary"
+        target="_blank"
+      >
+        Статья на Medium
+      </Link>
+      <Notes>
+        У MLS была задача создать динамический UI, который часто обновлялся.
+        Воспользовашись API React Apollo они установили требуемым запросам интервал в 60 секунд.
+        Количество кода уменьшилось более чем в 4 раза, места для багов стало меньше.
+      </Notes>
+    </Slide>
+
     <Slide bgColor="primary">
       <Heading fit textColor="secondary" margin="0 0 50px 0">
         Сравнение стоимости разработки решений
@@ -114,6 +140,7 @@ const Presentation = () => (
         Для подсчета средних значений временных затрат я пользовался выборкой из 20 задач
         с запросами к серверу, которые мне приходилось решать в разных проектах Qlean.
         Не все задачи подразумевали решение сложных проблем, выборка по ним меньше.
+        Эти расчеты наиболее правильно применять в относительном соотношении
       </Notes>
     </Slide>
     <Slide>
@@ -128,7 +155,9 @@ const Presentation = () => (
       <Notes>44 запроса в клинерском приложении, 58 в клиентском, 36 часов в пользу GraphQL</Notes>
     </Slide>
     <Slide>
-      <Heading size={4} textColor="secondary" margin="0 0 20px 0">Решение задач в минутах</Heading>
+      <Heading size={4} textColor="secondary" margin="0 0 20px 0">
+        Решение других задач в минутах
+      </Heading>
       <Table style={{ borderSpacing: '0 20px', color: colors.white }}>
         <TableRow style={{ color: colors.red }}>
           <TableHeaderItem>Задача</TableHeaderItem>
@@ -177,11 +206,45 @@ const Presentation = () => (
           Кеширование любой сущности при условии описании резолва
           Во время мутации мы просим сервер вернуть другие зависимые коллекции для синхронизации
           Оптимистичный апдейт описывается декларативно
+
+          В приложении, имеющим 50 запросов к серверу каждая из этих вещей
+          может усредненно может присутствовать в 20 экземеплярах
+          Ещё 45 часов в пользу GraphQL на приложение и 90 часов на два приложения
         </Notes>
       </Table>
     </Slide>
     <Slide bgColor="primary">
+      <Heading fit textColor="quaternary" margin="0 0 50px">
+        Обмен данными с сервером в цифрах
+      </Heading>
+      <Heading size={4} textColor="secondary" margin="0 0 50px">
+        Redux ~ 149 часов без дебага
+      </Heading>
+      <Heading size={4} textColor="secondary" margin="0 0 150px">
+        GraphQL ~ 24 часа без дебага
+      </Heading>
+      <Text textSize={25} textColor="tertiary">
+        Выборка составлена по приложениям Qlean для клинеров и клиентов
+      </Text>
+      <Notes>
+        К сожалению невозможно посчитать время, которые мы тратим на дебаг.
+        Однажды с неработающим линтером в HRM я порядка 3 часов искал ошибку в коде,
+        оказалось запятая прилипла к номерам линий в редакторе.
+        Про непойманные ошибки в промисах вообще отдельный разговор, да и про JS в целом
+      </Notes>
+    </Slide>
+
+    <Slide bgColor="primary">
       <Heading fit textColor="secondary" margin="0 0 50px 0">
+        Уменьшение связанности сервера и клиента
+      </Heading>
+      <Notes>
+        Что сказать
+      </Notes>
+    </Slide>
+
+    <Slide bgColor="primary">
+      <Heading fit textColor="secondary" margin="0 0 50px">
         Боли взаимодействия Redux с сервером
       </Heading>
       <Image src={images.reduxLogo} width={400}/>
@@ -192,7 +255,7 @@ const Presentation = () => (
     </Slide>
     <Slide bgColor="primary" textColor="tertiary">
       <Heading fit textColor="secondary" caps>
-        Redux Boilerplate для запроса к серверу
+        Запрос к серверу с помощью Redux
       </Heading>
       <List ordered>
         <ListItem>Создать селектор и константу</ListItem>
@@ -285,9 +348,17 @@ const Presentation = () => (
         <ListItem margin="25px 0">Серверная валидация форм</ListItem>
         <ListItem>Пагинация</ListItem>
         <ListItem margin="25px 0">Кеширование</ListItem>
-        <ListItem>Нормализация данных и ошибок</ListItem>
+        <ListItem>Нормализация данных</ListItem>
       </List>
+      <Notes>
+        После запроса нам в некоторых случаях надо до 5 запросов сделать, чтобы обновить другой UI
+        Ошибки нужно обрабатывать уже в саге, форматировать и только за тем передавать в компонент
+        Пагинация в Redux делается вручуную, приходится заводить локальный стейт под неё
+        Кеширование коллекций работает, но вот сущностей нет
+        Данные нужно нормализовать описывая схемы нормализации, чтобы ими можно было пользоваться
+      </Notes>
     </Slide>
+
     <Slide bgColor="primary">
       <Layout>
         <Image src={images.graphqlLogo} width={1000}/>
@@ -297,6 +368,9 @@ const Presentation = () => (
     <Slide bgColor="#e6e8ec">
       <Heading size={4} textColor="secondary" margin="0 0 50px">Как работает GraphQL</Heading>
       <Image src={images.graphql} width={500}/>
+      <Notes>
+        Все работает очень просто, описываем данные запроса и сразу работаем с ними уже в компоненте
+      </Notes>
     </Slide>
     <Slide bgColor="primary" textColor="tertiary">
       <Heading fit textColor="secondary" caps>
@@ -328,6 +402,114 @@ const Presentation = () => (
       ]}
     />
 
+    <Slide bgColor="primary">
+      <Image src={images.apollo} width={1000}/>
+    </Slide>
+    <Slide bgColor="dracula">
+      <Heading size={4} textColor="secondary" margin="0 0 20px">Пагинация</Heading>
+      <CodePane
+        lang="jsx"
+        source={codeSamples.pagination}
+        theme="external"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      />
+      <Notes>
+        В Apollo Client есть реализация из коробки курсорной и оффсетной пагинации.
+        Все что нам нужно - это описать каким образом мержить данные дополнительных запросов
+      </Notes>
+    </Slide>
+    <Slide bgColor="dracula">
+      <Heading size={4} textColor="secondary" margin="0 0 20px 0">Инвалидация</Heading>
+      <CodePane
+        lang="js"
+        source={codeSamples.mutation}
+        theme="external"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      />
+      <Notes>
+        Допустим мы отправили какие-то данные на сервер и нужно перезапросить несколько коллекций
+        В Redux мы делаем для этого дополнительные запросы к серверу (есть кейсы, когда их 5-6)
+        В GraphQL мы просто описываем данные, которые хотим обновить с ответа на мутацию
+      </Notes>
+    </Slide>
+    <Slide bgColor="dracula">
+      <Heading size={4} textColor="secondary" margin="0 0 20px">Оптимистичный UI</Heading>
+      <CodePane
+        lang="jsx"
+        source={codeSamples.mutationComponent}
+        theme="external"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      />
+      <Notes>
+        Хороший UX - когда пользователю не приходится ждать ответа сервера.
+        Если пользователь обновил данные используя мобильный интеренет,
+        нам приходится сигнализировать ему крутилкой ожидание ответа сервера.
+        Мы можем оптимистично отреагировать на его запрос и обновить UI моментально.
+        В случаях, когда сервер лежит мы можем сообщить об этом пользователю
+        и попросить повторить свой запрос
+      </Notes>
+    </Slide>
+    <Slide bgColor="dracula">
+      <Heading size={4} textColor="secondary" margin="0 0 20px">Кеширование</Heading>
+      <CodePane
+        lang="jsx"
+        source={codeSamples.cache}
+        theme="external"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      />
+      <Notes>
+        Чтобы кешировать сущность нам просто нужно описать её резолвинг по id
+      </Notes>
+    </Slide>
+    <Slide bgColor="dracula">
+      <Heading size={4} textColor="secondary" margin="0 0 20px">Локальное состояние</Heading>
+      <CodePane
+        lang="jsx"
+        source={codeSamples.localState}
+        theme="external"
+        style={{ display: 'flex', justifyContent: 'center' }}
+      />
+      <Notes>
+        Мы любим Redux за то,
+        что он является единым источником правды и хранит все состояние приложения.
+        Apollo Client позволяет нам делать ровно тоже самое, только без описания редьюсеров.
+      </Notes>
+    </Slide>
+    <Slide bgColor="dracula">
+      <Heading size={4} textColor="secondary" margin="-30px 0 30px">Серверная валидация</Heading>
+      <iframe
+        src="https://codesandbox.io/embed/o151702ppz?autoresize=1&fontsize=20&eslint=1&view=editor"
+        style={{
+          width: '1200px',
+          height: '600px',
+          border: '0',
+          borderRadius: '4px',
+          overflow: 'hidden',
+          marginLeft: '-100px',
+        }}
+        sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
+      />
+    </Slide>
+
+    <Slide bgColor="primary" textColor="tertiary">
+      <Heading fit textColor="secondary">
+        Плавная миграция с Redux на GraphQL
+      </Heading>
+    </Slide>
+
+    <Slide bgColor="primary" textColor="tertiary">
+      <Heading fit textColor="secondary">
+        Внедрение GraphQL в клинерское приложение
+      </Heading>
+      <Notes>Стоит 80 часов - окупится через полгода при 10 задачах с серверными реквестами</Notes>
+    </Slide>
+
+    <Slide bgColor="primary" textColor="tertiary">
+      <Heading fit textColor="secondary" caps>
+        Q & A
+      </Heading>
+    </Slide>
+
     <Slide bgColor="tertiary">
       <BlockQuote>
         <Quote textColor="secondary">Example Quote</Quote>
@@ -335,17 +517,9 @@ const Presentation = () => (
       </BlockQuote>
     </Slide>
     <Slide>
-      <Heading size={4} textColor="secondary" margin="0 0 30px 0">Become a psycho using GIF</Heading>
+      <Heading size={4} textColor="secondary" margin="0 0 30px">Become a psycho using GIF</Heading>
       <Image src={images.psycho} width={900}/>
       <Notes>gifs work too</Notes>
-    </Slide>
-    <Slide>
-      <Heading size={5} textColor="secondary" margin="0 0 30px 0">Code Sample</Heading>
-      <CodePane
-        lang="jsx"
-        source={codeSamples.deck}
-        theme="external"
-      />
     </Slide>
   </Deck>
 );
